cfa8893021796060b89838973d3649a0dd512759
jmartinez163 <jmartinez163@mail.csuchico.edu>
2016-10-19 17:30:22 -0700

    no message

--- form-validation.js
+++ form-validation.js
@@ -1,24 +1,28 @@
-function validate(){
+function register(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
   var txt = userEntered.length;
   if(txt < 6 || userEntered.indexOf(" ")>=0){
-
+      function validateusername()
+  {
   document.getElementById("usernameError").innerHTML
  = "username must be six characters and no spaces.";
 document.getElementById("usernameError").classList.remove("hidden-message");
 document.getElementById("usernameError").classList.add("shown-message");
-document.getElementById("usernameGroup").classList.add("has-error");
+document.getElementById("usernameGroup").classList.remove("has-error");
+  window.alert("Username Entered!");
 }
-
+function validatePassword()
+  {
 var password = "password";
 var lowercasePassword = passEntered.toLowerCase();
 if(lowercasePassword == password){
   document.getElementById("passwordError").innerHTML = "password cannot be password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
-  document.getElementById("passwordGroup").classList.add("has-error");
+  document.getElementById("passwordGroup").classList.remove("has-error");
+    window.alert("Password Entered!");
   }
 
 else if(passEntered.length < 6 || passEntered.length > 20) {
@@ -26,4 +30,12 @@ else if(passEntered.length < 6 || passEntered.length > 20) {
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
 }
+}
+
+
+
+
+
+
+
 }
